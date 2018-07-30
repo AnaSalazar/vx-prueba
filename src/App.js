ab9c6da78ff905cb57ad46bf86fb7c4e03ee20b7
AnaSalazar <anastephany.salazarguzman@gmail.com>
2018-07-30 17:41:42 -0500

    Instala dependencias de VX y agrega data necesaria

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Prueba from './prueba';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          
+        <Prueba width="1000" height="800"/>
       </div>
     );
   }
